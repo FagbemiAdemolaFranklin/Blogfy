d86ca11a98b7c6467f7e79e6f04dbd1e924e9feb
Franklin <fraklinademola4@gmail.com>
2023-05-02 17:15:48 +0200

    Specified the port in the environment

--- index.js
+++ index.js
@@ -244,7 +244,7 @@ async function main(){
 
     });
 
-    app.listen(3000, function() {
+    app.listen(process.env.PORT || 3000, function() {
         console.log("Server started on port 3000.");
     });
 
